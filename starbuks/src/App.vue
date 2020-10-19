d17611c78ab20841a9c704c99faf6e79283542c0
李诵焘 <3210677428@qq.com>
2020-10-19 14:50:21 +0800

    底部通栏

--- starbuks/src/App.vue
+++ starbuks/src/App.vue
@@ -1,10 +1,47 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div class="nav-bar">
+        <el-menu
+          :default-active="activeIndex"
+          class="el-menu-demo"
+          mode="horizontal"
+          @select="handleSelect"
+        >
+          <el-menu-item >
+            <a href="#" class="home">
+              <img src="./assets/images/icon-home.svg" />
+              <div><span>主页</span></div>
+            </a>
+          </el-menu-item>
+          <el-menu-item >
+            <a href="#" class="stores">
+              <img src="./assets/images/icon-stores.svg" />
+              <div><span>门店</span></div>
+            </a>
+          </el-menu-item>
+          <el-menu-item >
+            <a href="#" class="account">
+              <img src="./assets/images/icon-account.svg" />
+              <div><span>我的账户</span></div>
+            </a>
+          </el-menu-item>
+          <el-menu-item >
+            <a href="#" class="menu">
+              <img src="./assets/images/icon-menu.svg" />
+              <div><span>菜单</span></div>
+            </a>
+          </el-menu-item>
+          <el-menu-item >
+            <a href="#" class="more">
+              <img src="./assets/images/icon-more.svg" />
+              <div><span>更多</span></div>
+            </a>
+          </el-menu-item>
+        </el-menu>
+      </div>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
@@ -18,14 +55,61 @@
 }
 
 #nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  .el-menu--horizontal > .el-menu-item.is-active {
+    border: none;
+  }
+  .nav-bar {
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    background: #fff;
+    .el-menu {
+      .home:hover{
+        img{
+         content: url("./assets/images/icon-home-active.svg");
+        }
+          color: #00A862;
+      }
+      .stores:hover{
+        img{
+         content: url("./assets/images/icon-stores-active.svg");
+        }
+          color: #00A862;
+      }
+      .account:hover{
+        img{
+         content: url("./assets/images/icon-account-active.svg");
+        }
+          color: #00A862;
+      }
+      .menu:hover{
+        img{
+         content: url("./assets/images/icon-menu-active.svg");
+        }
+          color: #00A862;
+      }
+      .more:hover{
+        img{
+         content: url("./assets/images/icon-more-active.svg");
+        }
+          color: #00A862;
+      }
+      li {
+        line-height: unset;
+        width: 20%;
+        height: 55px;
+        padding: 0;
+        a {
+          text-decoration: none;
+          // &.router-link-exact-active {
+          //   color: #42b983;
+          // }
+          span{
+            font-weight: 400;
+            font-size: 12px;
+          }
+        }
+      }
     }
   }
 }
