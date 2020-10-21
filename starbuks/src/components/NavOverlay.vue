a29c006b9d40599aa09d723f4a53e16434d6f75f
maoba <1455685074@qq.com>
2020-10-21 20:33:35 +0800

    头header

---
+++ starbuks/src/components/NavOverlay.vue
@@ -0,0 +1,34 @@
+<template>
+	<article class="nav-overlay menu-page">
+		<div class="nav-container">
+			<nav-header-overlay @clickedCloseMenu="closeMenu"></nav-header-overlay>
+			<nav-body-overlay></nav-body-overlay>
+			<nav-footer-overlay></nav-footer-overlay>
+		</div>
+	</article>
+</template>
+<script>
+
+	import NavHeaderOverlay from '@/components/navHeaderOverlay'
+	import NavBodyOverlay from '@/components/navBodyOverlay'
+	import NavFooterOverlay from '@/components/navFooterOverlay'
+
+	export default {
+		data(){
+			return {
+				
+
+			}
+		},
+		components: {
+			NavHeaderOverlay: NavHeaderOverlay,
+			NavBodyOverlay: NavBodyOverlay,
+			NavFooterOverlay: NavFooterOverlay
+		},
+		methods: {
+			closeMenu(){
+				this.$emit("closeMenuOverlay");
+			}
+		}
+	}
+</script>
--- starbuks/src/views/Home.vue
+++ starbuks/src/views/Home.vue
@@ -1,16 +1,138 @@
 <template>
-  <div class="home">
-   我是主页
+  <div>
+    <article class="wrapper">
+      <nav class="nav">
+        <nav-container
+          v-on:showMenuOverlay="showMenuExec"
+          v-show="!isshow"
+        >
+          <span slot="navbody">
+            <section class="body" v-if="!NickName">
+              <div class="account">
+                <div class="greetings">
+                  <span>心情惬意，来杯咖啡吧 ☕</span>
+                </div>
+                <div class="account-entry">
+                  <a
+                    href="javascript:;"
+                    class="sign-in button"
+                    @click="pageRedir(2)"
+                  >
+                    <span>登录</span>
+                  </a>
+                  <a
+                    href="javascript:;"
+                    class="register button"
+                    @click="pageRedir(4)"
+                  >
+                    <span>注册</span>
+                  </a>
+                </div>
+              </div>
+            </section>
+            <section class="body" v-if="NickName">
+              <div class="account">
+                <a
+                  href="javascript:;"
+                  class="logo logo-onlogin"
+                  @click="pageRedir(0)"
+                  v-if="mbMedia"
+                >
+                  <img src="@/assets/images/logo.svg" alt="星巴克" />
+                </a>
+                <div class="greetings onlogin" :class="{ withlogo: mbMedia }">
+                  <span>{{ loginGreetings[random] }}</span>
+                </div>
+                <div class="account-info clearfix">
+                  <!-- 此处需使用v-show,使用v-if会导致进度条无法渲染 -->
+                  <div
+                    class="user-stars clearfix"
+                    @click="pageRedir(2)"
+                    v-show="!loading"
+                  >
+                    <div class="star-level">
+                      <span>
+                        <strong>{{ StarsNum }}</strong
+                        >/{{ StarsOfNextLevNeed }}
+                      </span>
+                      <div class="gauge">
+                        <div class="gauge-fill"></div>
+                      </div>
+                    </div>
+                    <div class="star-icon">
+                      <img
+                        src="@/assets/icons/icon-star-white.svg"
+                        v-if="StarLev == 'White'"
+                      />
+                      <img
+                        src="@/assets/icons/icon-star-green.svg"
+                        v-if="StarLev == 'Green'"
+                      />
+                      <img
+                        src="@/assets/icons/icon-star-gold.svg"
+                        v-if="StarLev == 'Gold'"
+                      />
+                    </div>
+                  </div>
+                  <div
+                    class="user-rewards"
+                    @click="pageRedir(7)"
+                    v-show="!loading"
+                  >
+                    <img src="@/assets/icons/icon-gift.svg" /><span>{{
+                      MyRewardsNum
+                    }}</span>
+                  </div>
+                </div>
+                <div class="loading-svg center" v-if="loading">
+                  <img src="@/assets/loading-svg/loading-bubbles.svg" />
+                </div>
+              </div>
+            </section>
+          </span>
+        </nav-container>
+        <nav-overlay
+          v-on:closeMenuOverlay="closeMenuExec"
+          v-show="isshow"
+        >
+        </nav-overlay>
+      </nav>
+    </article>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import "./../assets/css/bootstrap.min.css";
+import "./../assets/css/swiper.min.css";
+import "./../assets/css/init.css";
+import "./../assets/css/styles.css";
+import NavContainer from "@/components/navContainer";
+import NavOverlay from "@/components/NavOverlay";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-
-  }
-}
+    NavContainer,
+    NavOverlay,
+  },
+  data(){
+    return{
+	isshow: false,
+    }
+  },
+  methods: {
+    showMenuExec() {
+      this.isshow = true;
+    },
+    closeMenuExec() {
+      this.isshow= false;
+    },
+  },
+  computed: {
+			NickName(){
+				return this.$store.state.NickName;
+			}
+		},
+};
 </script>
+
