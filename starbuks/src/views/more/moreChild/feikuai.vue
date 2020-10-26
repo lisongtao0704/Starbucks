ff73784ffc131e0d22064aa53adc985a12b05041
LHJ910412173 <910412173@qq.com>
2020-10-26 09:10:07 +0800

    冷浩杰

--- starbuks/src/views/more/moreChild/feikuai.vue
+++ starbuks/src/views/more/moreChild/feikuai.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="feikuai">
-      我是啡快
+    <nav class="head">
+      <div class="display-1">啡快™ － 在线点 到店取</div>
+    </nav>
+  <div>
+    <img src="@/assets/images/fkhead.jpg" alt="" class="jp1">
+    </div>
+    <div>
+    <img src="@/assets/images/fk2.jpg" alt="" class="jp2">
+    </div>
+    <div>
+    <img src="@/assets/images/fk3.jpg" alt="" class="jp3">
+    </div>
+    <div>
+    <img src="@/assets/images/fk4.jpg" alt="" class="jp4">
+    </div>
+    <div>
+    <img src="@/assets/images/fk5.jpg" alt="" class="jp5">
+    </div>  
   </div>
 </template>
 
@@ -10,5 +27,32 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.feikuai{
+      padding-bottom: 50px;
+}
+.display-1{
+font-size: 1.4rem;
+font-weight: 700;
+text-align: left;
+letter-spacing: 0.4px;
+margin-top: 1.2rem;
+margin-left: 1rem;
+}
+.jp1{
+  width: 100%;
+  margin-top: 1rem;
+}
+.jp2{
+  width: 100%;
+}
+.jp3{
+  width: 100%;
+}
+.jp4{
+  width: 100%;
+}
+.jp5{
+  width: 100%;
+}
 </style>
