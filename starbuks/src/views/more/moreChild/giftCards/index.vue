424be764bbbd47735058b5d41ed035f278a29c38
李诵焘 <3210677428@qq.com>
2020-10-31 21:04:43 +0800

    星礼卡结束

--- starbuks/src/views/more/moreChild/giftCards/index.vue
+++ starbuks/src/views/more/moreChild/giftCards/index.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="gift1">
     <div class="issue">
+      <span class="adv">广告</span>
       <span>星巴克星礼卡</span>
       <h2>心意相随</h2>
       <p>
         将星礼卡绑定至星享俱乐部帐户，使用星巴克App移动支付，即可加速积星，每消费40元获得一颗星星，比其他支付方式积星快25%。
       </p>
       <p>50元/100元/200元/300元/500元 （全国通用）</p>
-      <a href="#">常见问题</a>
+      <a href="/more/help">常见问题</a>
     </div>
     <div class="cards">
       <img src="../../../../assets/images/svc-cards.png" />
@@ -17,8 +18,121 @@
       <p>天猫商城</p>
       <h3>浓浓心意，送给特别的TA</h3>
       <img src="../../../../assets/images/svc-tmall-1.png" />
-      <a href="#">前往天猫购买</a>
+      <div></div>
+      <a
+        href="https://starbucks.world.tmall.com/p/rd993825.htm?&scene=taobao_shop"
+        >前往天猫购买</a
+      >
       <img src="../../../../assets/images/svc-tmall-2.png" />
+    </div>
+    <div class="scan">
+      <p>扫码赠送星礼卡</p>
+      <img src="../../../../assets/images/svc-say-1.png" />
+      <div></div>
+      <img src="../../../../assets/images/svc-say-2.png" />
+      <p>“用星说” 全新社交礼品平台，一个简单的小行动，即刻用心说出真情实意</p>
+    </div>
+    <div class="buy">
+      <h5>公司购买</h5>
+      <p>
+        一张星礼卡，将您的浓浓心意送给最重要的人。这份体贴而便捷的礼物替您在人际沟通中增进彼此的情感连接。
+      </p>
+      <p>
+        如您有公司采购需求，请拨打星巴克服务热线：<a href="#" class="phone"
+          >400-820-6998</a
+        >, 将有专人为您提供服务。
+      </p>
+      <a
+        href="https://www.starbucks.com.cn/help/legal/terms-of-starbucks-gift-card/"
+        class="rule"
+        >查看星礼卡章程</a
+      >
+      <div></div>
+      <img src="../../../../assets/images/svc-company.png" />
+    </div>
+    <div class="set">
+      <h3>星巴克星礼卡系列</h3>
+      <p>50元/100元/200元/300元/500元 （全国通用）</p>
+      <a
+        class="set-list"
+        href="https://detail.tmall.com/item.htm?spm=a1z10.5-b-s.w4011-14489348263.80.6991ce03pNZHD2&id=553531548399&rn=6652b9c3265bc0ba06640e2a115b55ef&abbucket=1"
+      >
+        <div class="bg1">
+          <span>品牌主题</span>
+        </div>
+        <article>
+          <p>香醇祝福</p>
+          <p>一直在身边</p>
+        </article>
+      </a>
+      <a
+        class="set-list"
+        href="https://detail.tmall.com/item.htm?spm=a1z10.5-b-s.w4011-14489348263.146.6991ce03pNZHD2&id=529444751731&rn=6652b9c3265bc0ba06640e2a115b55ef&abbucket=1"
+      >
+        <div class="bg2">
+          <span>生日快乐主题</span>
+        </div>
+        <article>
+          <p>生日祝福送给TA</p>
+          <p>香醇快乐一起分享</p>
+        </article>
+      </a>
+      <a
+        class="set-list"
+        href="https://detail.tmall.com/item.htm?spm=a1z10.5-b-s.w4011-14489348263.143.6991ce03pNZHD2&id=529537240640&rn=6652b9c3265bc0ba06640e2a115b55ef&abbucket=1"
+      >
+        <div class="bg3">
+          <span>感谢主题</span>
+        </div>
+        <article>
+          <p>一声谢谢一杯咖啡</p>
+          <p>让真挚的心意相随</p>
+          <p>让真挚的心意相随</p>
+        </article>
+      </a>
+      <a
+        class="set-list"
+        href="https://detail.tmall.com/item.htm?spm=a1z10.4-b-s.w5003-14489348342.6.20cc225fdxvC7n&id=538997440362&rn=cf979420232282e101855984d9f9b8b6&abbucket=18&scene=taobao_shop"
+      >
+        <div class="bg4">
+          <span>美人鱼主题</span>
+        </div>
+        <article>
+          <p>在一个萦绕香醇的惬意午后</p>
+          <p>让充满灵气的曼妙鱼尾</p>
+          <p>带 TA 追溯美人鱼的恣意踪迹</p>
+        </article>
+      </a>
+      <a
+        class="set-list"
+        href="https://detail.tmall.com/item.htm?spm=a1z10.4-b-s.w5003-14489348342.8.20cc225fdxvC7n&abtest=_AB-LR32-PR32&pvid=a7a7c2b0-20b1-4495-9935-f18ba57ed6d7&pos=9&abbucket=_AB-M32_B9&acm=03054.1003.1.1285741&id=522184405247&scm=1007.12559.61743.100200300000000&scene=taobao_shop"
+      >
+        <div class="bg5">
+          <span>咖啡主题</span>
+        </div>
+        <article>
+          <p>谢谢你送的咖啡</p>
+          <p>失落时为我打气</p>
+          <p>成功时为我开心</p>
+        </article>
+      </a>
+      <a
+        class="set-list"
+        href="https://detail.tmall.com/item.htm?spm=a1z10.4-b-s.w5003-14489348342.10.20cc225fdxvC7n&abtest=_AB-LR32-PR32&pvid=55577497-ad89-404e-a72f-8ada6027d0f5&pos=3&abbucket=_AB-M32_B9&acm=03054.1003.1.1285741&id=527896581853&scm=1007.12559.61743.100200300000000&scene=taobao_shop"
+      >
+        <div class="bg6">
+          <span>爱情主题</span>
+        </div>
+        <article>
+          <p>也许挚爱的模样很简单</p>
+          <p>你在闹我在笑，相看两不厌</p>
+          <p>深情藏心间</p>
+        </article>
+      </a>
+      <a
+        href="https://starbucks.world.tmall.com/p/rd993825.htm?&scene=taobao_shop"
+        >前往天猫购买</a
+      >
     </div>
   </div>
 </template>
@@ -35,7 +149,19 @@ export default {
   .issue {
     padding: 48px 32px 0;
     background-color: #00a862;
-
+    position: relative;
+    .adv {
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      padding: 0px 6px;
+      color: #fff;
+      background-color: #000;
+      font-size: 12px;
+      opacity: 0.6;
+      border-radius: 2px;
+      z-index: 999;
+    }
     span {
       font-size: 14px;
       color: rgba(255, 255, 255, 0.7);
@@ -90,19 +216,170 @@ export default {
     }
     img:last-of-type {
       width: 60%;
-       display: block;
-    margin: auto;
+      display: block;
+      margin: auto;
     }
-    a{
+    a {
       margin: 24px 0 0;
-    border: 1px solid #fff;
-    border-radius: 48px;
-    box-sizing: border-box;
-    font-weight: 500;
-    padding: 6px 18px;
-    color: #FFF;
-    font-size: 16px;
-    display: inline-block;
+      border: 1px solid #fff;
+      border-radius: 48px;
+      box-sizing: border-box;
+      font-weight: 500;
+      padding: 6px 18px;
+      color: #fff;
+      font-size: 16px;
+      display: inline-block;
+    }
+  }
+  .scan {
+    text-align: center;
+    background-image: linear-gradient(-180deg, #36b35f 3%, #00a862 98%);
+    p:first-of-type {
+      color: rgba(255, 255, 255, 0.7);
+      font-size: 14px;
+      font-weight: 700;
+      padding: 20px;
+    }
+    img:first-of-type {
+      width: 100px;
+    }
+    img:last-of-type {
+      width: 230px;
+      margin-top: 20px;
+    }
+    p:last-of-type {
+      color: #fff;
+      font-size: 16px;
+      font-weight: 400;
+      margin: 0;
+      padding: 30px;
+    }
+  }
+  .buy {
+    background-color: #edebe9;
+    text-align: center;
+    padding: 0 26px;
+    h5 {
+      color: rgba(0, 0, 0, 0.56);
+      font-size: 14px;
+      font-weight: 700;
+      padding: 30px 0;
+      margin: 0;
+    }
+    p {
+      color: rgba(0, 0, 0, 0.56);
+      font-size: 16px;
+      font-weight: 400;
+      margin-bottom: 30px;
+    }
+    .rule {
+      border: 1px solid #00a862;
+      display: block;
+      width: 160px;
+      margin: auto;
+      border-radius: 48px;
+      color: #00a862;
+      font-weight: 500;
+      padding: 6px 18px;
+    }
+    img {
+      margin: auto;
+      width: 50vw;
+      display: block;
+    }
+  }
+  .set {
+    text-align: center;
+    padding: 0 12px 10px;
+    background-color: #f7f7f7;
+    h3 {
+      font-size: 20px;
+      font-weight: 400;
+      color: #000;
+      margin: 0;
+      padding: 30px 0 15px;
+    }
+    p {
+      color: rgba(0, 0, 0, 0.56);
+      font-size: 14px;
+      font-weight: 700;
+      margin-bottom: 20px;
+    }
+    a.set-list {
+      background: #fff;
+      margin-bottom: 25px;
+      display: block;
+      border-radius: 2px;
+      box-shadow: 0 1px 1px 1px rgba(0, 0, 0, 0.12);
+      div.bg1 {
+        position: relative;
+        height: 168px;
+        background: url(/img/svc-series-1.42c9e542.png) center;
+        background-size: cover;
+      }
+      div.bg2 {
+        position: relative;
+        height: 168px;
+        background: url("../../../../assets/images/svc-series-2.png") center;
+        background-size: cover;
+      }
+      div.bg3 {
+        position: relative;
+        height: 168px;
+        background: url("../../../../assets/images/svc-series-3.png") center;
+        background-size: cover;
+      }
+      div.bg4 {
+        position: relative;
+        height: 168px;
+        background: url("../../../../assets/images/svc-series-4.png") center;
+        background-size: cover;
+      }
+      div.bg5 {
+        position: relative;
+        height: 168px;
+        background: url("../../../../assets/images/svc-series-5.png") center;
+        background-size: cover;
+      }
+      div.bg6 {
+        position: relative;
+        height: 168px;
+        background: url("../../../../assets/images/svc-series-6.png") center;
+        background-size: cover;
+      }
+      div {
+        span {
+          font-size: 14px;
+          background: #c2a661;
+          color: #fff;
+          position: absolute;
+          padding: 4px 12px;
+          left: 0px;
+          top: 6px;
+        }
+      }
+      article {
+        padding: 10px 20px 15px;
+        p {
+          color: rgba(0, 0, 0, 0.56);
+          font-size: 16px;
+          font-weight: 400;
+          margin: 0;
+          text-align: left;
+        }
+      }
+    }
+    a:last-of-type {
+      border: 1px solid #00a862;
+      border-radius: 48px;
+      display: block;
+      width: 137px;
+      text-align: center;
+      margin: auto;
+      color: #00a862;
+      font-size: 16px;
+      font-weight: 500;
+      padding: 6px 18px;
     }
   }
 }
